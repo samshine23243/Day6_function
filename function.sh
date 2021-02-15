208502c13b2783cc0e1f9c8667495ce73426f407
samshine <sameershinde23243@gmail.com>
2021-02-15 14:52:40 +0530

    example2 code added

--- function.sh
+++ function.sh
@@ -1,1 +1,20 @@
 echo "function problems"
+sum=0
+pallindrome () {
+read -p "Enter number:" N
+N1=$N
+while [ $N -gt 0 ]
+do
+   temp=$(($N%10))
+   sum=$(($sum*10+$temp))
+   N=$(($N/10))
+done
+if [ $N1 -eq $sum ]
+then
+   echo "its pallindrome"
+else
+   echo "not pallindrome"
+fi
+}
+
+pallindrome
